6a6c691b386e171ca652cfe3e6f09fd51dce5c8b
Famer-Y <famer2git@163.com>
2025-05-16 13:24:09 +0800

    feat: add shadcn

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'wxt';
 
+import tailwindcss from '@tailwindcss/vite'
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
@@ -12,4 +14,8 @@ export default defineConfig({
       "default_path": "sidepanel.html"
     }
   },
+  vite: () => ({
+    // envPrefix: ["VITE_", "WXT_"],
+    plugins: [tailwindcss()],
+  }),
 });
